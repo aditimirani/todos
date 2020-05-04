1d620742cef5a9cdfb5d139c36465415fff11c51
aditi.thakkar <aditi.thakkar@beteasy.com.au>
2020-05-04 13:57:05 +1000

    chore: repeating setup for many test

--- src/components/todo.test.js
+++ src/components/todo.test.js
@@ -15,25 +15,26 @@ describe("<Todo /> component Unit test", () => {
         text: 'buy milk'
 
     };
+    
+    let component ;
+    beforeEach(() => {
+    component = shallow(<Todo {...props} />);
+    })
   it("Should render 1 <Todo /> component", () => {
-    const component = shallow(<Todo {...props} />);
     expect(component).toHaveLength(1);
     expect(component.find('li')).toHaveLength(1)
   });
 
   it("Should render props correctly", () => {
-    const component = shallow(<Todo {...props} />);
     expect(component.props().children).toEqual('buy milk');
   });
 
   it("Should set props correctly", () => {
-    const component = shallow(<Todo {...props} />);
     component.setProps({ text: 'hello'});
     expect(component.props().children).toEqual('hello');
   });
 
   it("Should call onClick handler when todo component ic clicked", () => {
-    const component = shallow(<Todo {...props} />);
     component.simulate('click')
     expect(mockFn).toHaveBeenCalledTimes(1);
 
